4689eaefc869e98f0f21f6e55688fd01a258996d
Fultslop <fultslop@gmail.com>
2026-02-14 15:49:51 +0100

    changing test to use functions

--- tests/embedm_plugins/hello_world_plugin_test.py
+++ tests/embedm_plugins/hello_world_plugin_test.py
@@ -1,16 +1,12 @@
-import unittest
-
 from embedm.domain.directive import Directive
 from embedm.plugins.transformer_base import NoParams
 from embedm_plugins.hello_world_plugin import HelloWorldPlugin
 from embedm_plugins.hello_world_transformer import HelloWorldTransformer
 
-class HelloWorldPluginTest(unittest.TestCase):
+def test_transformer_execute():
+    transformer = HelloWorldTransformer()
+    assert transformer.execute(NoParams()) == 'hello embedded world!'
 
-    def test_transformer_execute(self):
-        transformer = HelloWorldTransformer()
-        self.assertEqual(transformer.execute(NoParams()), 'hello embedded world!')
-
-    def test_plugin_transform(self):
-        plugin = HelloWorldPlugin()
-        self.assertEqual(plugin.transform(Directive(type="hello_world")), 'hello embedded world!')
+def test_plugin_transform():
+    plugin = HelloWorldPlugin()
+    assert plugin.transform(Directive(type="hello_world")) == 'hello embedded world!'
